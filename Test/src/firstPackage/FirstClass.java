ed89199ae2c4c778c7b8b635ec79fc029b3de4ac
s.singla <s.singla@BDC7-L-4HKNLC2.dir.svc.accenture.com>
2017-01-12 18:32:11 +0530

    First Commit

---
+++ Test/src/firstPackage/FirstClass.java
@@ -0,0 +1,56 @@
+package firstPackage;
+
+import org.openqa.selenium.By;
+import org.testng.annotations.Test;
+
+import secondPackage.ReUsableFunctions;
+
+public class FirstClass {
+	
+	By username = By.xpath("//input[@id='Email']");
+	By nextButton = By.xpath("//input[@id='next']");
+	By password = By.xpath("//input[@id='Passwd']");
+	By loginButton = By.xpath("//input[@id='signIn']");
+	By name = By.name("uid");
+    
+	@Test
+	public  void method1() throws Exception {
+
+		ReUsableFunctions.initiateDriver("IE");
+		ReUsableFunctions.launchWebsite("https://www.google.com/mail");
+		ReUsableFunctions.enterTextValue(username, "sumitsingla.24@gmail.com");
+		ReUsableFunctions.clickWebElement(nextButton);
+		ReUsableFunctions.enterTextValue(password, "jyotisumit");
+		ReUsableFunctions.clickWebElement(loginButton);
+		ReUsableFunctions.waitForPageLoad();
+		ReUsableFunctions.closeAllInstance();
+		/*
+		ReUsableFunctions.initiateDriver("FireFox");
+		ReUsableFunctions.launchWebsite("http://demo.guru99.com/V4/");
+		ReUsableFunctions.enterTextValue(name, "Driver 1");
+		ReUsableFunctions.closeAllInstance();*/
+	}
+
+	/*@Test
+	public  void method2() throws Exception {
+		ReUsableFunctions.initiateDriver("Chrome");
+		ReUsableFunctions.launchWebsite("http://demo.guru99.com/V4/");
+		ReUsableFunctions.enterTextValue(name, "Driver 2");
+		Thread.sleep(2000);
+		ReUsableFunctions.closeAllInstance();
+	}    
+        
+	
+	@Test
+        public void method3() throws Exception {
+		ReUsableFunctions.initiateDriver("Chrome");
+		ReUsableFunctions.launchWebsite("http://demo.guru99.com/V4/");
+		ReUsableFunctions.enterTextValue(name, "Driver 3");
+		Thread.sleep(2000);
+		ReUsableFunctions.closeAllInstance();
+	}
+*/	
+
+
+
+}
